1427536a5300333b35bad45d6c164a7783213517
Miraz Ahmed <miraz37@gmail.com>
2020-05-18 20:01:58 -0400

    updated controller files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const session = require('express-session');
 //load the environment variable file
 require('dotenv').config({path:"./config/keys.env"});
 
-const productRoutes = require("./controllers/Products");
+const productRoutes = require("./controllers/products");
 const generalRoutes = require("./controllers/General");
 const userRoutes = require("./controllers/User");
 
